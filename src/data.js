7ee80a551b4d4edbe4614a7375ef5e3e841759a2
LoreBarrera <karenlbarrera@gmail.com>
2023-08-07 12:24:12 -0500

    estrcutura html hasta section parte 1

--- src/data.js
+++ src/data.js
@@ -1,9 +1,35 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+const dataPokemon = [
+  {
+    "num": "001",
+    "name": "bulbasaur",
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+  };
+  {
+    {
+      "num": "002",
+      "name": "ivysaur",
+      "img": "https://www.serebii.net/pokemongo/pokemon/002.png"
+    },
+    {
+      "num": "003",
+      "name": "venusaur",
+      "img": "https://www.serebii.net/pokemongo/pokemon/003.png"
+    }
+  }
+
+]
+
+
+
+
+
+export default datosPokemon;
