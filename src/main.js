840524e88ab6d98126f5795f93dbf67757bb3bea
Malui Gomez <luisagomezrodela@gmail.com>
2023-08-10 11:48:29 -0600

    cambios pokemon grid

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import pokedex from "./data.js"
+import pokedex from "./data.js";
 //console.log(pokedex.pokemon[0].type[0], pokedex.pokemon[0].type[1])
 // console.log(pokedex.pokemon[6].name, pokedex.pokemon[6].tyoe[0])
 
 function pokemonHtml(data) {
-    let html = ''
-    data.forEach(unoporuno => {
-        html = html + `
-    <div>
-        <img src="${unoporuno.img}" alt="${unoporuno.name}"> >
-        <p>"${unoporuno.name}</p>
+  let html = "";
+  data.forEach((unoporuno) => {
+    html =
+      html +
+      `
+    <div id= "pokemonGrid">  
+        <img src="${unoporuno.img}" alt="${unoporuno.name}"> 
+        <p>${unoporuno.name}</p>
     </div>
-    `
-    })
+    `;
+  });
 
-    return html;
+  return html;
 }
 
-const root = document.getElementById('root')
-root.innerHTML = pokemonHtml(pokedex.pokemon)
+const root = document.getElementById("root");
+root.innerHTML = pokemonHtml(pokedex.pokemon);
