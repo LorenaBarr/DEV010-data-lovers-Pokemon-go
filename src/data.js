484fd918ea2ed12399224bc71dd3906629bf52d0
LoreBarrera <karenlbarrera@gmail.com>
2023-08-15 13:00:02 -0500

    se agrego la funcionalidad del filtro, el input sugiere al usuario por lista los nombres de los pokemon, a medida en que van escribiendo, funcionalidad del boton search para que aparezca el pokemon sugerido, al css se le agrego un color rosa pastel al fondo, y a las cards se le puso un bording separado para que no fuera una lista

--- src/data.js
+++ src/data.js
@@ -4,23 +4,23 @@ import pokedex from './data/pokemon/pokemon.js';
 
 const pokeData = {
     filterByType: function (type) {
-        if (type === "") {
-            return pokedex.pokemon;
-        } else {
-            return pokedex.pokemon.filter(pokemon => pokemon.type.includes(type))
-        }
+      if (type === "") {
+        return pokedex.pokemon;
+      } else {
+        return pokedex.pokemon.filter(pokemon => pokemon.type.includes(type))
+      }
+    },
+  
+    getPokemonNames: function () {
+      return pokedex.pokemon.map(pokemon => pokemon.name);
+    },
+    
+    filterByPokemonName: function (name) {
+        return pokedex.pokemon.filter(pokemon => pokemon.name.toLowerCase() === name);
     }
 };
 
-
-
-
 export default pokeData;
-
-
-// const firePokemon = pokedex.filter(pokemon => pokemon.type === 'fire');
-
-
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+// Importar el módulo pokeData que contiene las funciones y datos de los Pokémon
 import pokeData from "./data.js";
 
+// Función para generar el HTML de los Pokémon
 function pokemonHtml(data) {
   let html = "";
   data.forEach((pokemon) => {
@@ -11,20 +13,52 @@ function pokemonHtml(data) {
     </div>
     `;
   });
-
   return html;
 }
 
+// Obtener la referencia a la sección donde se mostrarán los Pokémon
 const section = document.getElementById("pokemon-grid");
 
-// Mostrar todos los Pokémon inicialmente
-section.innerHTML = pokemonHtml(pokeData.filterByType(""));
-
+// Obtener referencias a los elementos relevantes del DOM
+const inputField = document.querySelector(".search input");
+const suggestionsList = document.querySelector(".suggestions");
+const searchButton = document.querySelector(".search button");
 const typeFilter = document.getElementById("typeFilter");
 const filterButton = document.getElementById("filterButton");
 
+// Obtener la lista de nombres de Pokémon de tu base de datos
+const pokemonNames = pokeData.getPokemonNames();
+
+// Manejar el evento de autocompletado cuando el usuario escribe
+inputField.addEventListener("input", () => {
+  const inputText = inputField.value.toLowerCase();
+  const filteredNames = pokemonNames.filter(name => name.toLowerCase().includes(inputText));
+  suggestionsList.innerHTML = "";
+  filteredNames.forEach(name => {
+    const suggestionItem = document.createElement("div");
+    suggestionItem.classList.add("suggestion-item");
+    suggestionItem.textContent = name;
+    suggestionItem.addEventListener("click", () => {
+      inputField.value = name;
+      suggestionsList.innerHTML = "";
+    });
+    suggestionsList.appendChild(suggestionItem);
+  });
+});
+
+// Manejar el evento de búsqueda cuando el usuario presiona el botón "Search"
+searchButton.addEventListener("click", () => {
+  const selectedPokemonName = inputField.value.toLowerCase();
+  const selectedPokemon = pokeData.filterByPokemonName(selectedPokemonName);
+  section.innerHTML = pokemonHtml(selectedPokemon);
+});
+
+// Manejar el evento de filtrado por tipo de Pokémon
 filterButton.addEventListener("click", () => {
   const selectedType = typeFilter.value;
   const filteredPokemon = pokeData.filterByType(selectedType);
   section.innerHTML = pokemonHtml(filteredPokemon);
 });
+
+// Mostrar todos los Pokémon inicialmente
+section.innerHTML = pokemonHtml(pokeData.filterByType(""));
