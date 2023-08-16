3ccbc8d1caab2f2a3a4f91757a48078fc32156ed
LoreBarrera <karenlbarrera@gmail.com>
2023-08-16 10:57:52 -0500

    implementacion de prueba unitaria con la funcionalidad de filtrado, presenta errores...

--- src/data.js
+++ src/data.js
@@ -3,21 +3,21 @@
 import pokedex from './data/pokemon/pokemon.js';
 
 const pokeData = {
-    filterByType: function (type) {
-      if (type === "") {
-        return pokedex.pokemon;
-      } else {
-        return pokedex.pokemon.filter(pokemon => pokemon.type.includes(type))
-      }
-    },
-  
-    getPokemonNames: function () {
-      return pokedex.pokemon.map(pokemon => pokemon.name);
-    },
-    
-    filterByPokemonName: function (name) {
-        return pokedex.pokemon.filter(pokemon => pokemon.name.toLowerCase() === name);
+  filterByType: function (type) {
+    if (type === "") {
+      return pokedex.pokemon;
+    } else {
+      return pokedex.pokemon.filter(pokemon => pokemon.type.includes(type))
     }
+  },
+
+  getPokemonNames: function () {
+    return pokedex.pokemon.map(pokemon => pokemon.name);
+  },
+
+  filterByPokemonName: function (name) {
+    return pokedex.pokemon.filter(pokemon => pokemon.name.toLowerCase() === name);
+  }
 };
 
 export default pokeData;
